5289de45088f95d73b71ebb55f31ad44f8f0f307
PAVAN GOPAL <pavangopal0905@gmail.com>
2020-09-14 12:19:47 +0530

    Updates

---
+++ precache-manifest.4ab624bfe2d9d289808e61c5d3f6823c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4d74ee8b18479326a181f576798076",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d13fb7a3eb867106e8e6",
+    "url": "/todo/static/js/2.c5880889.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c5880889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8a19175c4708a63656",
+    "url": "/todo/static/js/main.d2360db4.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
